f6495a6476b9ee84a2885eb7ebd66d066918b9d5
Maksim Trigolos <Maksim_Trigolos@epam.com>
2020-12-13 12:12:13 +0400

    fix: Replace cart api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://vdzgh6g3v4.execute-api.eu-west-1.amazonaws.com/dev',
   bff: ' https://tsz5wbnhm9.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://trigolos-cart-api-dev.us-east-1.elasticbeanstalk.com/api'
+  cart: 'http://trigolos-cart-api-dev.eu-west-1.elasticbeanstalk.com/api'
 };
 
 export default API_PATHS;
